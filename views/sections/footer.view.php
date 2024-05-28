744bb73f79cd13f69ede6996d981b6a7b29110c4
munenepeter <peternjega7@gmail.com>
2024-05-28 20:25:07 +0300

    chore: update homepage to laravel's

--- views/sections/footer.view.php
+++ views/sections/footer.view.php
@@ -1,41 +1,28 @@
-<section id="footer">
-    <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 grid md:grid-cols-5 grid-cols-2 mx-auto">
-        <div class="p-4">
-            <p style="font-family: 'Cedarville Cursive', cursive;" class="text-purple-900 ml-2 hover:text-orange-700 text-4xl font-black">Chungu</p>
-            <p class="text-purple-900 text-lg font-bold whitespace-nowrap hover:text-orange-700">Developers</p>
-        </div>
+<footer class="my-6">
+    <ul class="flex flex-wrap items-center justify-center gap-6 text-sm text-center text-purple-500">
+        <li>
+            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Careers</a>
+        </li>
 
-        <div class="md:p-4 p-2">
-            <div class="text-sm uppercase text-orange-700 font-bold">Useful Links</div>
-            <a class="my-3 block text-purple-900 hover:underline" href="/services">Services<span class="text-teal-900 text-xs p-1"></span></a>
-            <a class="my-3 block text-purple-900 hover:underline" href="/projects">Projects<span class="text-teal-900 text-xs p-1"></span></a>
-        </div>
-        <div class="md:p-4 p-2">
-            <div class="text-sm uppercase text-orange-700 font-bold">Support</div>
-            <a class="my-3 block text-purple-900 hover:underline" href="/help-center">Help Center <span class="text-teal-900 text-xs p-1"></span></a>
-            <a class="my-3 block text-purple-900 hover:underline" href="https://chungu.co.ke/privacy-policy">Privacy Policy <span class="text-teal-900 text-xs p-1"></span></a>
-        </div>
-        <div class="md:p-4 p-2">
-            <div class="text-sm uppercase text-orange-700 font-bold">Contact us</div>
-            <a class="my-3 block text-purple-900 hover:underline" href="/#">+2547 426 41376<br>+2547 980 55244<br> Nairobi, Kenya. <span class="text-teal-900 text-xs p-1"></span></a>
-        </div>
-        <!-- <div class="p-4">
-                <div class="text-sm uppercase text-orange-700 font-bold">Mail us</div>
-                <a class="my-1 block text-teal-900 hover:underline" href="mailto:info@chungu.co.ke ">peter@chungu.co.ke <span class="text-teal-900 text-xs p-1"></span></a>
-                <a class="block text-teal-900 hover:underline" href="mailto:info@chungu.co.ke ">munenenjega@gmail.com <span class="text-teal-900 text-xs p-1"></span></a>
-            </div> -->
+        <li>
+            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Privacy</a>
+        </li>
 
-    </div>
+        <li>
+            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Terms of service</a>
+        </li>
 
-
+        <li>
+            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Support</a>
+        </li>
+    </ul>
     <div class="pt-1">
         <div class="flex px-3 m-auto border-t text-gray-800 text-sm flex-col
       max-w-screen-lg items-center">
             <div class="my-5 text-center text-purple-900">&copy; 2020 - <?= date("Y") ?> All rights reserved | Chungu Developers</div>
         </div>
     </div>
-</section>
-<script src="./../../node_modules/flowbite/dist/flowbite.min.js"></script>
+</footer>
 </body>
 
 </html>
