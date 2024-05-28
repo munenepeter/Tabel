77e9e63fbd6f25365ae014891f8ad5efdd7f0601
munenepeter <peternjega7@gmail.com>
2024-05-28 12:27:02 +0300

    chore: remove robots.txt route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,3 @@ Router::get('', [PagesController::class, 'index']);
 //logs
 Router::get(':system:/logs', [SystemController::class, 'index']);
 Router::post(':system:/logs/delete', [SystemController::class, 'deleteLogs']);
-//robots
-Router::get('robots.txt', function () {
-    return require __DIR__ . "/robots.txt";
-});
