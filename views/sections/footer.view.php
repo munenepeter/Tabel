3225fed0f5e7bcbdec043aaa7d7d072351445740
munenepeter <peternjega7@gmail.com>
2024-05-29 20:26:03 +0300

    simplify footer

--- views/sections/footer.view.php
+++ views/sections/footer.view.php
@@ -1,21 +1,4 @@
 <footer class="my-6">
-    <ul class="flex flex-wrap items-center justify-center gap-6 text-sm text-center text-purple-500">
-        <li>
-            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Careers</a>
-        </li>
-
-        <li>
-            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Privacy</a>
-        </li>
-
-        <li>
-            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Terms of service</a>
-        </li>
-
-        <li>
-            <a class="transition hover:text-gray-800 focus:underline focus:outline-none" href="#">Support</a>
-        </li>
-    </ul>
     <div class="pt-1">
         <div class="flex px-3 m-auto border-t text-gray-800 text-sm flex-col
       max-w-screen-lg items-center">
