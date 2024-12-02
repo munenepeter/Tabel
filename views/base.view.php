d8bfcd73654917d9343ce18933cb9ecd4b5d75fa
Peter Munene <peternjega7@gmail.com>
2024-12-02 17:21:29 +0300

    chore: clean up

--- views/base.view.php
+++ views/base.view.php
@@ -27,20 +27,9 @@
     <script defer src="<?php asset("js/index.js") ?>"></script>
 
 </head>
+
 <body class="flex flex-col min-h-screen bg-white">
     <!-- Simple Navbar -->
-    <nav class="border-b border-gray-200">
-        <div class="px-4 mx-auto max-w-7xl">
-            <div class="flex justify-between h-16">
-                <div class="flex items-center">
-                    <a href="/" class="text-xl font-semibold text-primary-dark">Tabel</a>
-                </div>
-                <div class="flex items-center space-x-6">
-                    <a href="/docs" class="transition text-primary hover:text-primary-medium">Documentation</a>
-                    <a href="https://github.com/munenepeter/Tabel" class="transition text-primary hover:text-primary-medium">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include_once 'sections/nav.view.php' ?>
     <!-- Main Content -->
     <main class="flex-grow">
